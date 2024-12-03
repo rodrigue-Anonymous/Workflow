827d551f802d9ea82c5c75d150f4e599471a7a65
unknown <rodriguebet@gmail.com>
2024-12-03 11:05:11 +0100

    FINAL UPDATE

--- app/Mail/TaskAssigned.php
+++ app/Mail/TaskAssigned.php
@@ -2,11 +2,9 @@
 
 namespace App\Mail;
 
+use App\Models\Task; // Modèle de tâche
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Content;
-use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
 class TaskAssigned extends Mailable
@@ -18,9 +16,9 @@ class TaskAssigned extends Mailable
     /**
      * Create a new message instance.
      *
-     * @param  \App\Models\Task  $task
+     * @param Task $task
      */
-    public function __construct(Task $task)
+    public function __construct(Task $task) // ATTENTION : Typage correct ici
     {
         $this->task = $task;
     }
@@ -33,5 +31,7 @@ class TaskAssigned extends Mailable
     public function build()
     {
         return $this->subject('Nouvelle Tâche Assignée')
-                    ->view('emails.task_assigned');
-    }}
+                    ->view('emails.task_assigned') // Vue pour l'email
+                    ->with(['task' => $this->task]);
+    }
+}
